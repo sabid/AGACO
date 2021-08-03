a3e9fd69201dd89c8db35504f6c1889239a646de
Sabid Barahona <sabid.barahona@gmail.com>
2021-08-02 20:25:13 -0600

    Iniciado proyecto

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <QCoreApplication>
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    cout << "Hola mundo aqui" << endl;
+
+    return 0;
+
+
+}
